6ee8635293d986c64731eab9531ed75e44eb9afc
Onur <58780366+onurnafiguzel@users.noreply.github.com>
2023-02-16 11:32:55 +0300

    Blazor.Modal eklendi.

--- MealOrdering/Server/Startup.cs
+++ MealOrdering/Server/Startup.cs
@@ -1,3 +1,4 @@
+using Blazored.Modal;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +26,8 @@ namespace MealOrdering.Server
 
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddBlazoredModal();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
