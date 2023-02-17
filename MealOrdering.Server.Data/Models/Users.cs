167690831e5718e9f60b2ed70d2edea1651e8731
Onur <58780366+onurnafiguzel@users.noreply.github.com>
2023-02-17 11:56:55 +0300

    Users Sınıfı

---
+++ MealOrdering.Server.Data/Models/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MealOrdering.Server.Data.Models
+{
+    public class Users
+    {
+        public Guid Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
