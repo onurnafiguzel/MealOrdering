715b37a8df363d1cb4b6e1544e0d50975b23a0cd
Onur <58780366+onurnafiguzel@users.noreply.github.com>
2023-02-17 12:07:21 +0300

    OrdersItem sınıfı

---
+++ MealOrdering.Server.Data/Models/OrdersItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MealOrdering.Server.Data.Models
+{
+    public class OrdersItem
+    {
+        public Guid Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public Guid CreatedUserId { get; set; }
+        public Guid OrderId { get; set; }
+        public string Description { get; set; }
+    }
+}
