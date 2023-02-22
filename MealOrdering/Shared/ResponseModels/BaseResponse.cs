58181184e4ae5c771433c5668734335cb3211138
Onur Nafi Guzel <nafi.guzel@consultant.boyner.com.tr>
2023-02-22 22:35:12 +0300

    UserController oluşturuldu.

---
+++ MealOrdering/Shared/ResponseModels/BaseResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MealOrdering.Shared.ResponseModels
+{
+    public class BaseResponse
+    {
+        public BaseResponse()
+        {
+            Success = true;
+        }
+        public bool Success { get; set; }
+        public String Message { get; set; }
+
+        public void SetException(Exception exception)
+        {
+            Success = false;
+            Message = exception.Message;
+        }
+    }
+}
