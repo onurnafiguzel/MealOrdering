ef9177323b32eedaca11597b62b2fa9120e134bd
Onur Nafi Guzel <nafi.guzel@consultant.boyner.com.tr>
2023-03-17 20:49:39 +0300

    HttpClientExtension düzenlendi.

--- MealOrdering/Client/Utils/HttpClientExtension.cs
+++ MealOrdering/Client/Utils/HttpClientExtension.cs
@@ -1,8 +1,6 @@
 ﻿using MealOrdering.Shared.CustomExceptions;
 using MealOrdering.Shared.ResponseModels;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
@@ -11,15 +9,40 @@ namespace MealOrdering.Client.Utils
 {
     public static class HttpClientExtension
     {
-        public async static Task<T> GetServiceResponse<T>(this HttpClient httpClient, string url, bool ThrowWhenNotSuccess = false)
+        public async static Task<TResult> PostGetServiceResponseAsync<TResult, TValue>(this HttpClient Client, String Url, TValue Value, bool ThrowSuccessException = false)
         {
-            
-            var httpRes = await httpClient.GetFromJsonAsync<ServiceResponse<T>>(url);
+            var httpRes = await Client.PostAsJsonAsync(Url, Value);
 
-            if (!httpRes.Success && ThrowWhenNotSuccess)
-                throw new ApiException(httpRes.Message);
+            if (httpRes.IsSuccessStatusCode)
+            {
+                var res = await httpRes.Content.ReadFromJsonAsync<ServiceResponse<TResult>>();
 
-            return httpRes.Value;
+                if (!!res.Success)
+
+                    return !res.Success && ThrowSuccessException ? throw new ApiException(res.Message) : res.Value;
+            }
+
+            throw new HttpException(httpRes.StatusCode.ToString());
+        }
+
+        public async static Task<BaseResponse> PostGetBaseResponseAsync<TValue>(this HttpClient Client, String Url, TValue Value, bool ThrowSuccessException = false)
+        {
+            var httpRes = await Client.PostAsJsonAsync(Url, Value);
+
+            if (httpRes.IsSuccessStatusCode)
+            {
+                var res = await httpRes.Content.ReadFromJsonAsync<BaseResponse>();
+
+                return !res.Success && ThrowSuccessException ? throw new ApiException(res.Message) : res;
+            }
+
+            throw new HttpException(httpRes.StatusCode.ToString());
+        }
+        public async static Task<T> GetServiceResponseAsync<T>(this HttpClient Client, String Url, bool ThrowSuccessException = false)
+        {
+            var httpRes = await Client.GetFromJsonAsync<ServiceResponse<T>>(Url);
+
+            return !httpRes.Success && ThrowSuccessException ? throw new ApiException(httpRes.Message) : httpRes.Value;
         }
     }
 }
